850b11ce3e7c6fa3db169736ac0317c15d2b105d
tkShir <takashirono@gmail.com>
2026-02-15 20:13:42 +0900

    fix init script

--- db/init/001-create-app-user.sh
+++ db/init/001-create-app-user.sh
@@ -1,20 +1,16 @@
-#!/usr/bin/env sh
+#!/bin/sh
 set -eu
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" \
-  -v app_user="$APP_DB_USER" \
-  -v app_pass="$APP_DB_PASSWORD" \
-  <<'SQL'
+PGOPTIONS="-c app.app_user=$APP_USER -c app.app_pass=$APP_PASS" \
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<'SQL'
 DO $$
 BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = :'app_user') THEN
-    EXECUTE format('CREATE ROLE %I WITH LOGIN PASSWORD %L', :'app_user', :'app_pass');
+  IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = current_setting('app.app_user')) THEN
+    EXECUTE format('CREATE ROLE %I WITH LOGIN PASSWORD %L',
+      current_setting('app.app_user'),
+      current_setting('app.app_pass')
+    );
   END IF;
 END
 $$;
-
-GRANT CONNECT ON DATABASE :"POSTGRES_DB" TO :"app_user";
-GRANT USAGE ON SCHEMA public TO :"app_user";
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO :"app_user";
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO :"app_user";
 SQL
