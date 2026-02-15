97229bde2afa9ca47559a691dd0a1eff36d9520a
tkShir <takashirono@gmail.com>
2026-02-15 20:34:13 +0900

    update db script for privilege

--- db/init/002-grants.sql
+++ db/init/002-grants.sql
@@ -5,9 +5,10 @@ GRANT USAGE ON SCHEMA public TO app_user;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app_user;
 GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA public TO app_user;
 
--- 3) future tables/sequences created by the migration role (often "postgres")
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public
+-- 3) future tables/sequences created by the migration role (often "postgres") 
+-- TODO: extract the role name from .env
+ALTER DEFAULT PRIVILEGES FOR ROLE user IN SCHEMA public
   GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO app_user;
 
-ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public
+ALTER DEFAULT PRIVILEGES FOR ROLE user IN SCHEMA public
   GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO app_user;
